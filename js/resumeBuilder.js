3475ed8ce3e10d6decda86e6f994c0c8fdb9029a
7sa <7sa@users.noreply.github.com>
2016-04-29 14:10:45 -0400

    last look before deleting practice exercises

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,51 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var awesomeThoughts = "I am yo mama and I am AWESOME!";
+console.log(awesomeThoughts);
+$("#main").append(funThoughts);
+
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+$("#main").append(funThoughts);
+
+var name = "7SA";
+var role = "web dev";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
+var skills = ["awesomeness"];
+$("#main").append(skills);
+
+var bio = {
+	name: "Philip J. Fry",
+	role: "Delivery Boy",
+	contact: "philipjfry@planetexpress.net",
+	picURL: "https://github.com/7sa/frontend-nanodegree-resume/blob/master/images/fry.jpg",
+	welMSG: "Oh, NOW I get it!",
+	skills: skills
+};
+
+$("#main").append(bio.welMSG);
+
+var work = {
+	position: "Delivery Boy",
+	employer: "Planet Express",
+	yearsWorked: 7,
+	city: "New New York"
+};
+
+var education  = {
+	name: "Mars University",
+	years: "several days",
+	city: "New York"
+};
+
+$("#main").append(work["position"]);
+
+$("#main").append(education.name);
